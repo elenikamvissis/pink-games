bf67e9da5c8ec3040222d3ca3e2aa8ea31aeb89f
Eleni Kamvissis <eleni.kamvissis@docly.com>
2020-08-21 10:15:04 +0200

    Snake

--- src/components/snake/index.js
+++ src/components/snake/index.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './index.css'
+import * as helpers from './helpers.js'
 
 function Snake() {
+  const [game, setGame] = useState(helpers.generateGame())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setGame(helpers.tick), 400)
+    return () => clearInterval(intervalId)
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress)
+    return () => window.removeEventListener('keydown', handleKeyPress)
+  }, [])
+
+  function handleKeyPress(event) {
+    let newDir
+    switch (event.keyCode) {
+      case 37:
+      case 65:
+        newDir = 'left'
+        break
+      case 38:
+      case 87:
+        newDir = 'up'
+        break
+      case 39:
+      case 68:
+        newDir = 'right'
+        break
+      case 40:
+      case 83:
+        newDir = 'down'
+        break
+    }
+    if (newDir) {
+      setGame((oldGame) => {
+        return { ...oldGame, snake: { ...oldGame.snake, dir: newDir } }
+      })
+    }
+  }
+
+  const cells = []
+  for (let y = 0; y < helpers.height; y++) {
+    for (let x = 0; x < helpers.width; x++) {
+      // same as {x: x, y: y}
+      const cell = { x, y }
+      let className = ''
+      if (helpers.isEqual(cell, game.snake.head)) {
+        className = ' head'
+      } else if (helpers.isEqual(cell, game.food)) {
+        className = ' food'
+      } else if (
+        game.snake.tail.some((tailCell) => helpers.isEqual(cell, tailCell))
+      ) {
+        className = ' tail'
+      }
+
+      cells.push(
+        <div key={x + '-' + y} className={'snake-cell' + className}></div>
+      )
+    }
+  }
+
   return (
-    <div className='text-container'>
-      <h2>Snake</h2>
+    <div className='game-container'>
+      <div className='snake-grid'>{cells}</div>
     </div>
   )
 }
